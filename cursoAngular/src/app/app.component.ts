ff9d10172f7c1c06ed5412d802cb21b584541847
CamilaSilva <camiladasilvaassis1@gmail.com>
2022-09-23 17:51:20 -0300

    feature: testando novas funcionalidades

--- cursoAngular/src/app/app.component.ts
+++ cursoAngular/src/app/app.component.ts
@@ -5,6 +5,20 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
+  Username: string = "Camila";
+
+  data = {
+      name: "Anderson",
+      age: 29
+  }
+
+  colors = {
+      color1: "Purple",
+      color2: "Black",
+      color3: "Red"
+  }
+
   title = 'cursoAngular';
 }
--- cursoAngular/src/app/app.module.ts
+++ cursoAngular/src/app/app.module.ts
@@ -3,11 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { FirstComponentComponent } from './components/first-component/first-component.component';
+import { SecondComponentComponent } from './components/second-component/second-component.component';
+import { ParentDataComponent } from './components/parent-data/parent-data.component';
+import { DiretivasComponent } from './components/diretivas/diretivas.component';
+import { IfRenderComponent } from './components/if-render/if-render.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    FirstComponentComponent
+    FirstComponentComponent,
+    SecondComponentComponent,
+    ParentDataComponent,
+    DiretivasComponent,
+    IfRenderComponent
   ],
   imports: [
     BrowserModule
